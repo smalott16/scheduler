0d366ddc913051d1bd7b48f72756e73bac847140
Spencer Malott <spencermalott16@gmail.com>
2021-10-26 19:33:06 -0700

    w7d2 activities all committed here. We storybook tested the button, daylist, and daylist items. We then implemented the daylist and daylist item components and called them in the application file

---
+++ src/components/DayListItem.js
@@ -0,0 +1,34 @@
+import React from "react";
+import classNames from 'classnames'
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+  const { name, spots, selected, setDay } = props
+  
+  let isfull = false
+  if (spots === 0) {
+    isfull = true;
+  }
+
+  const formatSpots = (spots) => {
+    if (spots === 1) {
+      return `${spots} spot`; 
+    } else if (spots === 0) {
+      return "no spots";
+    } else {
+      return  `${spots} spots`;
+    }
+  }
+
+  const styling = classNames("day-list__item", {
+    "day-list__item--selected": selected,
+    "day-list__item--full": isfull
+  });
+  
+  return (
+    <li className={styling} onClick={() => setDay(name)}>
+      <h2 className="text--regular">{name}</h2> 
+      <h3 className="text--light">{formatSpots(spots)} remaining</h3>
+    </li>
+  );
+}
